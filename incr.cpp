34628eed85d0c31d00b1daa28ce80912fa215143
dev-swarna <swarnamayee564@gmail.com>
2026-07-05 08:53:00 +0530

    In

---
+++ incr.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+    // int a = x++;
+    // cout<<a<<endl;
+    // cout<<x;
+    int b = ++x;
+    cout<<b<<endl;
+    cout<<x;
+
+
+        // x++;
+        //cout<<x<<endl;
+
+        // ++x;
+        // cout<<x<<endl;
+
+
+        // int y = ++x + ++x;
+        // cout<<"Val of y:"<<y<<endl; //14
+        // int z = x++ + ++x;
+        // cout<<"Val of z:"<<z<<endl; //12
+
+}
